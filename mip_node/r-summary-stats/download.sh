5f4a57b2e628fb6a6b251b7a2cf988147f67e570
Ludovic Claude <ludovic.claude54@gmail.com>
2015-11-24 11:14:31 +0100

    Download scripts builds also the dependent projet to ensure freshness

--- mip_node/r-summary-stats/download.sh
+++ mip_node/r-summary-stats/download.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cp ../../../hbpsummarystats/hbpsummarystats_0.0.0.9000_R_x86_64-pc-linux-gnu.tar.gz downloads/
+SUMMARY_STATS_DIR="../../../../mip-functions/hbpsummarystats"
+
+(cd $SUMMARY_STATS_DIR && ./build.sh && ./dist.sh)
+cp $SUMMARY_STATS_DIR/hbpsummarystats_0.0.0.9000_R_x86_64-pc-linux-gnu.tar.gz downloads/
