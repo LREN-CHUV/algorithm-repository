9238515fa6b98e3bee927852cf12225167763c10
Ludovic Claude <ludovic.claude54@gmail.com>
2016-01-18 13:57:35 +0100

    Simplify dev script

--- mip_federation/r-linear-regression/dev.sh
+++ mip_federation/r-linear-regression/dev.sh
@@ -12,82 +12,48 @@ function cheat_sheet() {
   echo "source(\"/src/main.R\")"
   echo "  Perform the computation"
   echo
-  echo "lintr::lint_package()"
+  echo "lintr::lint(\"/src/main.R\")"
   echo "  Checks the style of the source code"
-  echo
-  echo "devtools::load_all()"
-  echo "  Load the code in the current project"
-  echo
-  echo "devtools::document()"
-  echo "  Generates the documentation"
-  echo
-  echo "devtools::use_testthat()"
-  echo "  Setup the package to use testthat"
   echo
   echo "-----------------------------------------"
 
 }
 
-if [[ "$USER" == "vagrant" ]]; then
-
-  cheat_sheet
-
-  docker run -i -t --rm \
-    -v $(pwd):/src/ \
-    -v $(pwd)/tests:/src/tests/ \
-    -e JOB_ID=002 \
-    -e NODE=Test \
-    -e PARAM_query="select * from job_result_nodes where job_id='002'" \
-    -e PARAM_colnames="tissue1_volume" \
-    -e IN_JDBC_DRIVER=org.postgresql.Driver \
-    -e IN_JDBC_JAR_PATH=/usr/lib/R/libraries/postgresql-9.4-1201.jdbc41.jar \
-    -e IN_JDBC_URL="jdbc:postgresql://172.17.42.1:31432/postgres" \
-    -e IN_JDBC_USER=postgres \
-    -e IN_JDBC_PASSWORD=vagrantadmin \
-    -e OUT_JDBC_DRIVER=org.postgresql.Driver \
-    -e OUT_JDBC_JAR_PATH=/usr/lib/R/libraries/postgresql-9.4-1201.jdbc41.jar \
-    -e OUT_JDBC_URL="jdbc:postgresql://172.17.42.1:31432/analytics" \
-    -e OUT_JDBC_USER=analytics \
-    -e OUT_JDBC_PASSWORD=neuroinfo \
-    registry.federation.mip.hbp/mip_tools/r-interactive R
-
+echo "Starting the results database..."
+../../tests/analytics-db/start-db.sh
+echo "Starting the federation database..."
+../../tests/dummy-federation/start-db.sh
+  
+sleep 2
+  
+if groups $USER | grep &>/dev/null '\bdocker\b'; then
+  DOCKER="docker"
 else
-
-  ../../tests/analytics-db/start-db.sh
-  ../../tests/dummy-federation/start-db.sh
-  
-  sleep 2
-  
-  if groups $USER | grep &>/dev/null '\bdocker\b'; then
-    DOCKER="docker"
-  else
-    DOCKER="sudo docker"
-  fi
-
-  cheat_sheet
-
-  $DOCKER run -i -t --rm \
-    --link dummyfederation:indb \
-    --link analyticsdb:outdb \
-    -v $(pwd)/:/src/ \
-    -v $(pwd)/tests:/src/tests/ \
-    -e JOB_ID=002 \
-    -e NODE=dev \
-    -e PARAM_query="select * from job_result_nodes where job_id='002'" \
-    -e PARAM_colnames="tissue1_volume" \
-    -e IN_JDBC_DRIVER=org.postgresql.Driver \
-    -e IN_JDBC_JAR_PATH=/usr/lib/R/libraries/postgresql-9.4-1201.jdbc41.jar \
-    -e IN_JDBC_URL="jdbc:postgresql://indb:5432/postgres" \
-    -e IN_JDBC_USER=postgres \
-    -e IN_JDBC_PASSWORD=test \
-    -e OUT_JDBC_DRIVER=org.postgresql.Driver \
-    -e OUT_JDBC_JAR_PATH=/usr/lib/R/libraries/postgresql-9.4-1201.jdbc41.jar \
-    -e OUT_JDBC_URL="jdbc:postgresql://outdb:5432/postgres" \
-    -e OUT_JDBC_USER=postgres \
-    -e OUT_JDBC_PASSWORD=test \
-    registry.federation.mip.hbp/mip_tools/r-interactive R
-
-  ../../tests/analytics-db/stop-db.sh
-  ../../tests/dummy-federation/stop-db.sh
-
+  DOCKER="sudo docker"
 fi
+
+cheat_sheet
+
+$DOCKER run -i -t --rm \
+  --link dummyfederation:indb \
+  --link analyticsdb:outdb \
+  -v $(pwd)/:/src/ \
+  -v $(pwd)/tests:/src/tests/ \
+  -e JOB_ID=002 \
+  -e NODE=dev \
+  -e PARAM_query="select * from job_result_nodes where job_id='002'" \
+  -e PARAM_colnames="tissue1_volume" \
+  -e IN_JDBC_DRIVER=org.postgresql.Driver \
+  -e IN_JDBC_JAR_PATH=/usr/lib/R/libraries/postgresql-9.4-1201.jdbc41.jar \
+  -e IN_JDBC_URL="jdbc:postgresql://indb:5432/postgres" \
+  -e IN_JDBC_USER=postgres \
+  -e IN_JDBC_PASSWORD=test \
+  -e OUT_JDBC_DRIVER=org.postgresql.Driver \
+  -e OUT_JDBC_JAR_PATH=/usr/lib/R/libraries/postgresql-9.4-1201.jdbc41.jar \
+  -e OUT_JDBC_URL="jdbc:postgresql://outdb:5432/postgres" \
+  -e OUT_JDBC_USER=postgres \
+  -e OUT_JDBC_PASSWORD=test \
+  registry.federation.mip.hbp/mip_tools/r-interactive R
+
+../../tests/analytics-db/stop-db.sh
+../../tests/dummy-federation/stop-db.sh
