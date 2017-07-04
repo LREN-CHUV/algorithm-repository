45f8f3dccc29d5b28544ae64c76ef7f58f8ab739
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-07-04 16:57:35 +0200

    update dev.sh

--- python-histograms/dev.sh
+++ python-histograms/dev.sh
@@ -5,13 +5,13 @@ docker run \
 -e "NODE=federation" \
 -e "PARAM_variables=lefthippocampus" \
 -e "PARAM_covariables=" \
--e "PARAM_grouping=dx" \
--e "PARAM_meta={\"lefthippocampus\":{\"type\":\"real\"},\"dx\":{\"type\":\"polynominal\",\"enumerations\":[{\"code\":\"CN\",\"label\":\"CN\"},{\"code\":\"MCI\",\"label\":\"MCI\"},{\"code\":\"AD\",\"label\":\"AD\"}]}}" \
--e "PARAM_query=select lefthippocampus, dx from ADNI_MERGE where lefthippocampus is not null and dx is not null" \
--e "IN_JDBC_URL=jdbc:postgresql://192.168.0.1:65432/postgres" \
--e "IN_JDBC_USER=postgres" \
--e "IN_JDBC_PASSWORD=test" \
--e "OUT_JDBC_URL=jdbc:postgresql://192.168.0.1:5432/postgres" \
--e "OUT_JDBC_USER=postgres" \
--e "OUT_JDBC_PASSWORD=test" \
+-e "PARAM_grouping=alzheimerbroadcategory" \
+-e "PARAM_meta={\"lefthippocampus\":{\"description\":\"\",\"methodology\":\"lren-nmm-volumes\",\"label\":\"Left Hippocampus\",\"code\":\"lefthippocampus\",\"units\":\"cm3\",\"length\":20,\"type\":\"real\"},\"alzheimerbroadcategory\":{\"enumerations\":[{\"code\":\"AD\",\"label\":\"Alzheimer's disease\"},{\"code\":\"CN\",\"label\":\"Cognitively Normal\"},{\"code\":\"Other\",\"label\":\"Other\"}],\"description\":\"There will be two broad categories taken into account. Alzheimer's disease (AD) in which the diagnostic is 100% certain and \\\"Other\\\" comprising the rest of Alzheimer's related categories. The \\\"Other\\\" category refers to Alzheime's related diagnosis which origin can be traced to other pathology eg. vascular. In this category MCI diagnosis can also be found. In summary, all Alzheimer's related diagnosis that are not pure.\",\"methodology\":\"mip-cde\",\"label\":\"Alzheimer Broad Category\",\"code\":\"alzheimerbroadcategory\",\"type\":\"polynominal\"}}" \
+-e "PARAM_query=select lefthippocampus,alzheimerbroadcategory from merged_data where lefthippocampus is not null and alzheimerbroadcategory is not null and alzheimerbroadcategory != 'Other'" \
+-e "IN_JDBC_URL=jdbc:postgresql://172.18.0.1:5432/features" \
+-e "IN_JDBC_USER=features" \
+-e "IN_JDBC_PASSWORD=featurespwd" \
+-e "OUT_JDBC_URL=jdbc:postgresql://172.18.0.1:5432/woken" \
+-e "OUT_JDBC_USER=woken" \
+-e "OUT_JDBC_PASSWORD=wokenpwd" \
 hbpmip/python-histograms compute
