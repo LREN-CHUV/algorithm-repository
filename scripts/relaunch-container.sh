5dbf084fa39305d1f0c8b22a7e78fe1d8671d4f2
Ludovic Claude <ludovic.claude54@gmail.com>
2017-08-17 20:10:51 +0200

    Update relaunch script

--- scripts/relaunch-container.sh
+++ scripts/relaunch-container.sh
@@ -2,9 +2,12 @@
 
 container_id=$1
 
-env=$(docker inspect $container_id | jq --raw-output '.. | .Env? | .[]? | (" -e \"" + . + "\"")')
+env_file="$(mktemp)"
+docker inspect $container_id | jq --raw-output '.. | .Env? | .[]?' > "$env_file"
 
 image=$(docker inspect $container_id | jq --raw-output '[.. | .Config? | .Image?] | map(select(. != null))[0]')
 
-docker run -it --rm $env $image
+docker run -it --rm --env-file "$env_file" "$image"
+
+rm "$env_file"
 
