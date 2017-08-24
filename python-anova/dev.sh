865769d763550f5c34fae912e9c618789cc32e10
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-08-24 17:44:40 +0200

    use a third independent variable for testing

--- python-anova/dev.sh
+++ python-anova/dev.sh
@@ -4,10 +4,10 @@ docker run \
 -e "JOB_ID=$(uuidgen)" \
 -e "NODE=federation" \
 -e "PARAM_variables=iq" \
--e "PARAM_covariables=score_math_course1,score_math_course2" \
+-e "PARAM_covariables=college_math,score_math_course1,score_math_course2" \
 -e "PARAM_grouping=" \
--e "PARAM_meta={\"iq\":{\"code\":\"iq\",\"type\":\"real\"},\"score_math_course1\":{\"code\":\"score_math_course1\",\"type\":\"real\"},\"score_math_course2\":{\"code\":\"score_math_course2\",\"type\":\"real\"}}" \
--e "PARAM_query=select iq,score_math_course1,score_math_course2 from sample_data where iq is not null and score_math_course1 is not null and score_math_course2 is not null" \
+-e "PARAM_meta={\"iq\":{\"code\":\"iq\",\"type\":\"real\"},\"college_math\":{\"code\":\"college_math\",\"type\":\"real\"},\"score_math_course1\":{\"code\":\"score_math_course1\",\"type\":\"real\"},\"score_math_course2\":{\"code\":\"score_math_course2\",\"type\":\"real\"}}" \
+-e "PARAM_query=select iq,college_math,score_math_course1,score_math_course2 from sample_data where iq is not null and college_math is not null and score_math_course1 is not null and score_math_course2 is not null" \
 -e "IN_JDBC_URL=jdbc:postgresql://172.18.0.1:5432/features" \
 -e "IN_JDBC_USER=postgres" \
 -e "IN_JDBC_PASSWORD=test" \
