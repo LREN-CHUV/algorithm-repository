15151820924ad347d5551661656603d580295408
Ludovic Claude <ludovic.claude@laposte.net>
2017-09-13 00:38:42 +0200

    CircleCI: improve build

--- java-rapidminer-knn/build.sh
+++ java-rapidminer-knn/build.sh
@@ -1,8 +1,15 @@
-#!/bin/bash -e
-if groups $USER | grep &>/dev/null '\bdocker\b'; then
+#!/usr/bin/env bash
+set -e
+
+if [[ $NO_SUDO || -n "$CIRCLECI" ]]; then
+  CAPTAIN="captain"
+elif groups $USER | grep &>/dev/null '\bdocker\b'; then
   CAPTAIN="captain"
 else
   CAPTAIN="sudo captain"
 fi
 
-$CAPTAIN build
+BUILD_DATE=$(date -Iseconds) \
+  VCS_REF=$(git describe --tags --dirty) \
+  VERSION=$(git describe --tags --dirty) \
+  $CAPTAIN build
