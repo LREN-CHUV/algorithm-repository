bd881872abcae6991130fe1edc5690f71ccbc839
Ludovic Claude <ludovic.claude54@gmail.com>
2017-09-13 13:51:34 +0200

    build scripts: cd to script directory

--- java-rapidminer-knn/build.sh
+++ java-rapidminer-knn/build.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
-set -e
+
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     cd -P "$( dirname "$SOURCE" )"
+     pwd
+}
+
+cd "$(get_script_dir)"
 
 if [[ $NO_SUDO || -n "$CIRCLECI" ]]; then
   CAPTAIN="captain"
