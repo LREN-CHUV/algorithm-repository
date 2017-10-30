43042d92880bc83e6726733a81168c5774284b8a
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-10-30 11:49:53 +0100

    add python-linear-regression algorithm

---
+++ python-linear-regression/README.md
@@ -0,0 +1,22 @@
+[![CHUV](https://img.shields.io/badge/CHUV-LREN-AF4C64.svg)](https://www.unil.ch/lren/en/home.html) [![DockerHub](https://img.shields.io/badge/docker-hbpmip%2Fpython--linear-regression-008bb8.svg)](https://hub.docker.com/r/hbpmip/python-linear-regression/)
+[![ImageVersion](https://images.microbadger.com/badges/version/hbpmip/python-linear-regression.svg)](https://hub.docker.com/r/hbpmip/python-linear-regression/tags "hbpmip/python-linear-regression image tags")
+[![ImageLayers](https://images.microbadger.com/badges/image/hbpmip/python-linear-regression.svg)](https://microbadger.com/#/images/hbpmip/python-linear-regression "hbpmip/python-linear-regression on microbadger")
+
+# Python linear-regression
+
+This is a Python implementation of linear-regression.
+
+
+## Build (for contributors)
+
+Run: `./build.sh`
+
+
+## Test (for contributors)
+
+Run: `./tests/test.sh`
+
+
+## Publish (for contributors)
+
+Run: `./publish.sh`
---
+++ python-linear-regression/linear_regression.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from io_helper import io_helper
+
+import logging
+import json
+
+from statsmodels.formula.api import ols
+
+DEFAULT_DOCKER_IMAGE = "python-linear-regression"
+
+
+def main():
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+
+    # Read inputs
+    inputs = io_helper.fetch_data()
+    dep_var = inputs["data"]["dependent"][0]
+    inped_vars = inputs["data"]["independent"]
+
+    # Check dependent variable type (should be continuous)
+    if dep_var["type"]["name"] not in ["integer", "real"]:
+        logging.warning("Dependent variable should be continuous !")
+        return None
+
+    # Extract data and parameters from inputs
+    data = format_data(inputs["data"])
+
+    # Compute linear-regression and generate PFA output
+    linear_regression_results = json.dumps(compute_linear_regression(dep_var, inped_vars, data))
+
+    # Store results
+    io_helper.save_results(linear_regression_results, '', 'application/highcharts+json')
+
+
+def format_data(input_data):
+    all_vars = input_data["dependent"] + input_data["independent"]
+    data = {v["name"]: v["series"] for v in all_vars}
+    return data
+
+
+def compute_linear_regression(dep_var, indep_vars, data):
+    formula = generate_formula(dep_var, indep_vars)
+    logging.info("Formula: %s" % formula)
+    lm = ols(data=data, formula=formula).fit()
+    logging.info(lm.summary())
+    return {
+        "coef": dict(lm.params),
+        "std_err": dict(lm.bse),
+        "t_values": dict(lm.tvalues),
+        "p_values": dict(lm.pvalues)
+    }
+
+
+def generate_formula(dep_var, indep_vars):
+    op = " + "
+    dep_var = dep_var["name"]
+    indep_vars = [v["name"] if v["type"]["name"] in ["integer", "real"]
+                  else str.format("C(%s)" % v["name"]) for v in indep_vars]
+    indep_vars = op.join(indep_vars)
+    indep_vars = indep_vars.strip(op)
+    return str.format("%s ~ %s" % (dep_var, indep_vars))
+
+
+if __name__ == '__main__':
+    main()
