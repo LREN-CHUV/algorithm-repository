dbafb7b11486e0d1b8606731459469d5ba91520a
Mirco <mirco.nasuti@chuv.ch>
2018-02-05 22:37:09 +0100

    add logs to python-statistics

--- python-statistics/statistics.py
+++ python-statistics/statistics.py
@@ -11,14 +11,21 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     # Read inputs
+    logging.info("Fetching data...")
     inputs = io_helper.fetch_data()
 
     # Load data into a Pandas dataframe
+    logging.info("Loading data into a Pandas Dataframe...")
     data_dict = {inputs['data']['dependent'][0]['name']: inputs['data']['dependent'][0]['series']}
     for variable in inputs['data']['independent']:
         data_dict.update({variable['name']: variable['series']})
     df = pandas.DataFrame(data_dict)
+
+    # Generate results
+    logging.info("Generating results...")
     io_helper.save_results(df.describe().to_json(), '', 'application/json')
+
+    logging.info("DONE")
 
 
 if __name__ == '__main__':
