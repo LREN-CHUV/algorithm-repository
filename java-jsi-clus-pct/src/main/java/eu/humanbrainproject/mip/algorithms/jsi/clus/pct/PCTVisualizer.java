fb7ce710fea0eb7ec8add72802ef62421283e876
Martin Breskvar <martin.breskvar@ijs.si>
2018-02-23 18:42:40 +0100

    java-jsi-clus-pct visualization update

--- java-jsi-clus-pct/src/main/java/eu/humanbrainproject/mip/algorithms/jsi/clus/pct/PCTVisualizer.java
+++ java-jsi-clus-pct/src/main/java/eu/humanbrainproject/mip/algorithms/jsi/clus/pct/PCTVisualizer.java
@@ -5,65 +5,87 @@ import si.ijs.kt.clus.algo.tdidt.ClusNode;
 import si.ijs.kt.clus.model.test.NodeTest;
 
 /**
- * 
  * @author Martin Breskvar
- * 
- * This class produces javascript for PCT visualization
+ *     <p>This class produces javascript for PCT visualization
  */
 public class PCTVisualizer extends ClusVisualizationSerializer<ClusNode> {
 
-	private void visualizationRecursive(ClusNode model, StringBuilder nodes, StringBuilder edges) {
-		NodeTest test = model.getTest();
+  private void visualizationRecursive(ClusNode model, StringBuilder nodes, StringBuilder edges) {
+    NodeTest test = model.getTest();
 
-		// add this node to nodes
-		if (model.atBottomLevel()) {
-			// add leaf node
-			nodes.append(String.format("nodes.push({id: %d, label: '%s'});", model.getID(),
-					model.getTargetStat().getPredictString()));
-		} else {
-			// add intermediate node
-			nodes.append(String.format("nodes.push({id: %d, label: '%s'});", model.getID(), test.getTestString()));
-		}
+    // add this node to nodes
+    if (model.atBottomLevel()) {
+      // add leaf node
+      nodes.append(
+          String.format(
+              "nodes.push({id: %d, label: '%s'});",
+              model.getID(), model.getTargetStat().getPredictString()));
+    } else {
+      // add intermediate node
+      nodes.append(
+          String.format("nodes.push({id: %d, label: '%s'});", model.getID(), test.getTestString()));
+    }
 
-		nodes.append(System.lineSeparator());
+    nodes.append(System.lineSeparator());
 
-		// add edges to children of this node to edges
-		for (int i = 0; i < model.getNbChildren(); i++) {
-			ClusNode child = (ClusNode) model.getChild(i);
+    // add edges to children of this node to edges
+    for (int i = 0; i < model.getNbChildren(); i++) {
+      ClusNode child = (ClusNode) model.getChild(i);
 
-			String lbl = i % 2 == 0 ? "No" : "Yes";
+      String lbl = i % 2 == 0 ? "No" : "Yes";
 
-			edges.append(
-					String.format("edges.push({from: %d, to: %d, label: '%s'});", model.getID(), child.getID(), lbl));
-			edges.append(System.lineSeparator());
+      edges.append(
+          String.format(
+              "edges.push({from: %d, to: %d, label: '%s'});", model.getID(), child.getID(), lbl));
+      edges.append(System.lineSeparator());
 
-			visualizationRecursive(child, nodes, edges);
-		}
-	}
+      visualizationRecursive(child, nodes, edges);
+    }
+  }
 
-	private String getVisJSCode() {
-		return "var container = document.getElementById('visualization');" + "var data = {" + "  nodes: nodes,"
-				+ "  edges: edges" + "};" + "var options = {" + "  layout: {" + "  hierarchical: {"
-				+ "     sortMethod: layoutMethod" + "   }" + " }," + " edges: {" + "   smooth: true,"
-				+ "   arrows: {to : true}" + " }" + "};" + "network = new vis.Network(container, data, options);";
-	}
+  private String getVisJSCode() {
+    return "var container = document.getElementById('visualization');"
+        + "var data = {"
+        + "  nodes: nodes,"
+        + "  edges: edges"
+        + "};"
+        + "var options = {"
+        + "  layout: {"
+        + "  hierarchical: {"
+        + "     sortMethod: layoutMethod"
+        + "   }"
+        + " },"
+        + " edges: {"
+        + "   smooth: true,"
+        + "   arrows: {to : true}"
+        + " }"
+        + "};"
+        + "network = new vis.Network(container, data, options);";
+  }
 
-	private String visualization(ClusNode model) {
+  private String visualization(ClusNode model) {
 
-		model.numberCompleteTree();
+    model.numberCompleteTree();
 
-		StringBuilder nodes = new StringBuilder();
-		StringBuilder edges = new StringBuilder();
+    StringBuilder nodes = new StringBuilder();
+    StringBuilder edges = new StringBuilder();
 
-		nodes.append("var nodes=[]; var edges=[];" + System.lineSeparator());
+    nodes.append("var nodes=[]; var edges=[];" + System.lineSeparator());
 
-		visualizationRecursive(model, nodes, edges);
+    if (model.atBottomLevel()) {
+      nodes.append(
+          String.format(
+              "nodes.push({id: %d, label: '%s'});",
+              model.getID(), model.getTargetStat().getPredictString()));
+    } else {
+      visualizationRecursive(model, nodes, edges);
+    }
 
-		return nodes.toString() + edges.toString() + getVisJSCode();
-	}
+    return nodes.toString() + edges.toString() + getVisJSCode();
+  }
 
-	@Override
-	public String getVisualizationString(ClusNode model) {
-		return visualization(model);
-	}
+  @Override
+  public String getVisualizationString(ClusNode model) {
+    return visualization(model);
+  }
 }
