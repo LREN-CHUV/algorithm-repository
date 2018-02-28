cf84d0dbf3451cf976d5b91ddcaa17be5f3c19b7
Ludovic Claude <ludovic.claude54@gmail.com>
2018-02-28 15:40:13 +0100

    Fix end of files

--- r-3c/main.R
+++ r-3c/main.R
@@ -4,28 +4,28 @@
 #' This query will be the same for all nodes.
 #'
 #' The environment variables are:
-#' 
-#' * Input Parameters (for 3c):  
-#'   - PARAM_query  : SQL query producing the dataframe to analyse  
+#'
+#' * Input Parameters (for 3c):
+#'   - PARAM_query  : SQL query producing the dataframe to analyse
 #'   - PARAM_variables : The variables to pass to y1 (Disease Diagnosis)
 #'   - PARAM_covariables1 : The variables to pass to X1 (Clinical Measures)
 #'   - PARAM_covariables2 : The variables to pass to X2 (Potential Biomarkers)
 #'   - PARAM_* : any other parameters to pass to the R function. See example in the docker-compose file.
-#' * Execution context:  
-#'    - JOB_ID : ID of the job  
-#'    - NODE : Node used for the execution of the script  
-#'    - IN_DBI_DRIVER   : Class name of the DBI driver for input data  
-#'    - IN_DBI_DBNAME     : Database name for the database connection for input data  
-#'    - IN_DBI_HOST     : Host name for the database connection for input data  
-#'    - IN_DBI_PORT     : Port number for the database connection for input data  
-#'    - IN_DBI_PASSWORD : Password for the database connection for input data  
-#'    - IN_DBI_USER     : User for the database connection for input data  
-#'    - OUT_DBI_DRIVER   : Class name of the DBI driver for output data  
-#'    - OUT_DBI_DBNAME     : Database name for the database connection for output data  
-#'    - OUT_DBI_HOST     : Host name for the database connection for output data  
-#'    - OUT_DBI_PORT     : Port number for the database connection for output data  
-#'    - OUT_DBI_USER     : User for the database connection for output data  
-#'    - OUT_DBI_PASSWORD : Password for the database connection for output data  
+#' * Execution context:
+#'    - JOB_ID : ID of the job
+#'    - NODE : Node used for the execution of the script
+#'    - IN_DBI_DRIVER   : Class name of the DBI driver for input data
+#'    - IN_DBI_DBNAME     : Database name for the database connection for input data
+#'    - IN_DBI_HOST     : Host name for the database connection for input data
+#'    - IN_DBI_PORT     : Port number for the database connection for input data
+#'    - IN_DBI_PASSWORD : Password for the database connection for input data
+#'    - IN_DBI_USER     : User for the database connection for input data
+#'    - OUT_DBI_DRIVER   : Class name of the DBI driver for output data
+#'    - OUT_DBI_DBNAME     : Database name for the database connection for output data
+#'    - OUT_DBI_HOST     : Host name for the database connection for output data
+#'    - OUT_DBI_PORT     : Port number for the database connection for output data
+#'    - OUT_DBI_USER     : User for the database connection for output data
+#'    - OUT_DBI_PASSWORD : Password for the database connection for output data
 
 pdf(NULL) # do not plot
 library(rmipadaptor)
@@ -58,7 +58,7 @@ var_list <- get_PARAM_env_vars_into_R_list()
 ## remap variables to fit CCC functions
 columns1 <- if (var_list$covariables1 == "") stop("missing PARAM_covariables1") else unlist(strsplit(var_list$covariables1, split = ","))
 columns2 <- if (var_list$covariables2 == "") stop("missing PARAM_covariables2") else unlist(strsplit(var_list$covariables2, split = ","))
-y <- var_list$variables 
+y <- var_list$variables
 ## convert to numeric the following input variables:
 numeric_inputs <- c()
 for (i in seq_along(var_list))
@@ -76,14 +76,12 @@ new_y <- C2_results[[3]]
 
 C3_results <- C3(PBx = data[,columns2], newy = new_y,
                  feature_selection_method = "RF",
-                 classification_method="RF") 
+                 classification_method="RF")
 
 # table(new_y, C3_results[[2]])
 
 model_as_pfa <- pfa(model)
 write_pfa(model_as_pfa,file = "/tmp/model.pfa")
 model_string <- paste0(readLines(con = "/tmp/model.pfa"),collapse = "\n")
-saveResults(results = model_string,shape = "pfa") 
+saveResults(results = model_string,shape = "pfa")
 disconnectdbs()
-
-
