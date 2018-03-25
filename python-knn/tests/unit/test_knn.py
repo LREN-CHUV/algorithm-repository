735e1f68d41176fe1fa2b7c25877e5d78c2fbab8
Marigold <mojmir.vinkler@gmail.com>
2018-03-25 13:16:47 +0200

    handle NULL values

--- python-knn/tests/unit/test_knn.py
+++ python-knn/tests/unit/test_knn.py
@@ -40,3 +40,23 @@ def test_compute_classification(mock_save_results, mock_get_results, mock_fetch_
     from titus.genpy import PFAEngine
     engine, = PFAEngine.fromJson(pfa_dict)
     engine.action({'stress_before_test1': 10., 'iq': 10.})
+
+
+@mock.patch('knn.io_helper.fetch_data')
+@mock.patch('knn.io_helper.save_results')
+@mock.patch('sys.exit')
+def test_compute_empty(mock_exit, mock_save_results, mock_fetch_data):
+    # create mock objects from database
+    data = fx.inputs_classification()
+
+    # all NULLs
+    data['data']['independent'][0]['series'] = [None] * len(data['data']['independent'][0]['series'])
+
+    mock_fetch_data.return_value = data
+
+    compute()
+
+    mock_exit.assert_called_once_with(1)
+    mock_save_results.call_args[0][0] == (
+        '', 'No data left after removing NULL values, cannot fit model.', 'text/plain+error'
+    )
