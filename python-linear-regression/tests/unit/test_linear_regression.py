565523f6d2811e3daae00e6ea276a79ee4d02a1a
Ludovic Claude <ludovic.claude@laposte.net>
2018-03-29 00:51:07 +0200

    Upgrading linear regression for mip-helper 0.4.0

--- python-linear-regression/tests/unit/test_linear_regression.py
+++ python-linear-regression/tests/unit/test_linear_regression.py
@@ -7,7 +7,7 @@ def test_main():
     io_helper = mock.MagicMock()
     with mock.patch.dict('sys.modules', io_helper=io_helper):
         from linear_regression import main
-        io_helper.io_helper.fetch_data.return_value = fx.inputs
+        mip_helper.io_helper.fetch_data.return_value = fx.inputs
         main()
         # TODO: fix numerical errors
-        assert io_helper.io_helper.save_results.assert_called_with(json.dumps(fx.outputs), '', 'application/json')
+        assert mip_helper.io_helper.save_results.assert_called_with(json.dumps(fx.outputs), '', 'application/json')
