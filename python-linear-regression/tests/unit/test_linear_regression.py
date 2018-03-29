1bb8870faf95e09d97ccbba99d338dc3fa9df01d
Marigold <mojmir.vinkler@gmail.com>
2018-03-29 12:23:34 +0200

    fix unit tests for linear regression

--- python-linear-regression/tests/unit/test_linear_regression.py
+++ python-linear-regression/tests/unit/test_linear_regression.py
@@ -1,13 +1,12 @@
 import mock
 import json
 from . import fixtures as fx
+from linear_regression import main
 
 
-def test_main():
-    io_helper = mock.MagicMock()
-    with mock.patch.dict('sys.modules', io_helper=io_helper):
-        from linear_regression import main
-        mip_helper.io_helper.fetch_data.return_value = fx.inputs
-        main()
-        # TODO: fix numerical errors
-        assert mip_helper.io_helper.save_results.assert_called_with(json.dumps(fx.outputs), '', 'application/json')
+@mock.patch('linear_regression.io_helper.fetch_data')
+@mock.patch('linear_regression.io_helper.save_results')
+def test_main(mock_save_results, mock_fetch_data):
+    mock_fetch_data.return_value = fx.inputs
+    main()
+    mock_save_results.assert_called_with(json.dumps(fx.outputs), '', 'application/json')
