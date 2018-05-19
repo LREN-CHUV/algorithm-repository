ec8baea569aea61cce7af5191b4877121e3fb87e
Martin Breskvar <martin.breskvar@ijs.si>
2018-05-19 23:12:56 +0200

    java-jsi-clus-rm publish.sh update

--- java-jsi-clus-rm/publish.sh
+++ java-jsi-clus-rm/publish.sh
@@ -110,7 +110,7 @@ BUILD_DATE=$(date -Iseconds) \
   $CAPTAIN push target_image --branch-tags=false --commit-tags=false --tag $updated_version
 
 ## Notify Microbadger
-curl -XPOST https://hooks.microbadger.com/images/hbpmip/java-jsi-clus-rm/H0gB-LPzhTkFcQofYqFTMa_VUnk=
+curl -XPOST https://hooks.microbadger.com/images/hbpmip/java-jsi-clus-rm/ej5DlQyTZSbxij9kF3zskHEHcJY=
 
 # Notify on slack
 sed "s/USER/${USER^}/" $WORKSPACE/slack.json > $WORKSPACE/.slack.json
